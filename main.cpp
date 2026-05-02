ec7c07294497416d29ce4eb03a2974861fa0c1c7
tatchatunwc-del <tatchatun.wc@gmail.com>
2026-05-02 15:48:44 +1000

    Task 3: update class hierarchy implementation
    Task 3: update class hierarchy implementation

--- main.cpp
+++ main.cpp
@@ -1,10 +1,51 @@
+class Book {
+protected:
+    string title, author, isbn;
+    bool availability;
+
+public:
+    Book() {}
+
+    Book(string t, string a, string i) {
+        title = t;
+        author = a;
+        isbn = i;
+        availability = true;
+    }
+
+    virtual void displayBookDetails() {
+        cout << "Title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "ISBN: " << isbn << endl;
+        cout << "Status: " << (availability ? "Available" : "Borrowed") << endl;
+    }
+
+    bool borrowBook() {
+        if (availability) {
+            availability = false;
+            return true;
+        }
+        return false;
+    }
+
+    string getISBN() {
+        return isbn;
+    }
+};
+
 class HardcopyBook : public Book {
 private:
     string shelfNumber;
 
 public:
-    void setShelf(string s) {
-        shelfNumber = s;
+    HardcopyBook(string t, string a, string i, string shelf)
+        : Book(t, a, i) {
+        shelfNumber = shelf;
+    }
+
+    void displayBookDetails() override {
+        Book::displayBookDetails();
+        cout << "Shelf: " << shelfNumber << endl;
     }
 };
 
@@ -13,7 +54,13 @@ private:
     string licenseEndDate;
 
 public:
-    void setLicense(string d) {
-        licenseEndDate = d;
+    EBook(string t, string a, string i, string date)
+        : Book(t, a, i) {
+        licenseEndDate = date;
+    }
+
+    void displayBookDetails() override {
+        Book::displayBookDetails();
+        cout << "License Ends: " << licenseEndDate << endl;
     }
 };
